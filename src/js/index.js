7233c5d658ef74536e6e8226bad99db50115b6d8
Leticia Soares <leticiabgmr@gmail.com>
2022-07-20 13:20:08 -0300

    Desafio Formulário

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,27 @@
-const todoItem = document.querySelectorAll('.items')
+const todoItem = document.querySelectorAll('.item')
 const botaoEnviar = document.querySelector('.botao')
 
-botaoEnviar.addEventListener('click', 
-        function (event){
-        event.preventDefault()
+todoItem.forEach(input  => {
+    input.addEventListener('input', () => {
+        if (input.value != "") {
+            input.classList.add('correto')
+            input.classList.remove('erro')
+            input.nextElementSibling.classList.remove('erro')
+        } else {
+            input.classList.remove('correto') 
+        }
+    })   
 })
 
-
-function enviarDados() {
-    for (const items of todoItem) {
-        const item = items.querySelector('.item')
-        const error = items.querySelector('.campo')
-
-        if (item.value === "") {
-            item.classList.add('errado')
-            item.classList.remove('correto')
-            error.classList.add('msg-errado')
-        } else {
-            item.classList.remove('errado')
-            item.classList.add('correto')
-            error.classList.remove('msg-errado')
-        }
-    }
-}
-
+botaoEnviar.addEventListener('click', (e) => {
+    todoItem.forEach(input => {
+            if (input.value === ""){
+                e.preventDefault()
+                input.classList.add('erro')
+                input.nextElementSibling.classList.add('erro')
+            } else {
+                input.classList.remove('erro')
+                input.nextElementSibling.classList.remove('erro')
+            }
+        })
+    })
